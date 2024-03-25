fbfb60e3e0749c8c5703adb4accfc9949ae9283a
dragatuso <danielaragatuso@gmail.com>
2024-03-24 20:11:43 -0600

    Complete Requirement 1

--- Code Files/api_keys.py
+++ Code Files/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "65990fea975c329e8efd659280e05816"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+# geoapify_key = "YOUR KEY HERE"
